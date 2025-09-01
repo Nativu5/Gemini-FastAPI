8376aaff7d1c681164708c5a06910fa8d626b81a
Shen Mintao <cx330.shen@autocore.ai>
2025-09-01 14:35:09 +0800

    fix: add logger info

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import uvicorn
 from app.main import create_app
 from app.utils import g_config, setup_logging
 import os
+from loguru import logger
+
 app = create_app()
 
 if __name__ == "__main__":
@@ -10,12 +12,13 @@ if __name__ == "__main__":
     setup_logging(level=g_config.logging.level)
 
 
+
     key_path = "/certs/privkey.pem"
     cert_path = "/certs/fullchain.pem"
     # 检查证书文件是否存在且配置了路径
     if os.path.exists(key_path) and os.path.exists(cert_path):
         # 如果证书存在，则以 HTTPS 模式运行
-        print("Starting server in HTTPS mode...")
+        logger.info("Starting server in HTTPS mode...")
         uvicorn.run(
             app,
             host=g_config.server.host,
@@ -26,7 +29,7 @@ if __name__ == "__main__":
         )
     else:
         # 否则，以 HTTP 模式运行
-        print("Starting server in HTTP mode (SSL certificates not found or configured)...")
+        logger.info("Starting server in HTTP mode (SSL certificates not found or configured)...")
         uvicorn.run(
             app,
             host=g_config.server.host,
